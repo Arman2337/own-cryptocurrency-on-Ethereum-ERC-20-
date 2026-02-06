cb46863b8677b690f540ccd310a4b485fbd199f9
Arman2337 <23it119@charusat.edu.in>
2026-02-06 15:45:56 +0530

    9. ending token sales

--- contracts/ShkTokenSale.sol
+++ contracts/ShkTokenSale.sol
@@ -31,5 +31,14 @@ contract ShkTokenSale {
 
         Sell(msg.sender, _numberOfTokens);
     }
-    
+
+    function endSale() public {
+        require(msg.sender == admin);
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
+
+        // UPDATE: Let's not destroy the contract here
+        // Just transfer the balance to the admin
+        admin.transfer(address(this).balance);
+    }
+
 }
