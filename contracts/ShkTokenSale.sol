1cda8abbbb4e37aceca10546e50ffe5d00caaa28
Arman2337 <23it119@charusat.edu.in>
2026-02-06 15:18:54 +0530

    8. purchasing ERC-20 token

--- contracts/ShkTokenSale.sol
+++ contracts/ShkTokenSale.sol
@@ -5,10 +5,31 @@ contract ShkTokenSale {
     address public admin;
     ShkToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(
+        address indexed _buyer,
+        uint256 _amount
+    );
 
     function ShkTokenSale(ShkToken _tokenContract , uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice; // Price of each token in wei (0.001 Ether)
     }
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "Multiplication overflow");
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
+        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        tokensSold += _numberOfTokens;
+
+        Sell(msg.sender, _numberOfTokens);
+    }
+    
 }
