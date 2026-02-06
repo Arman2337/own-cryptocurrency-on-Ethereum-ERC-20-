823266297f336f88c4440949c38f3955f00b6d43
Arman2337 <23it119@charusat.edu.in>
2026-02-06 13:54:01 +0530

    creating the erc-20 token smart contract

--- contracts/ShkToken.sol
+++ contracts/ShkToken.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.4.26;
 
 contract ShkToken {
     string public name = "Shk Token";
+    string public symbol = "Shk";
+    string public standard = "Shk Token v1.0";
     uint public totalSupply;
 
-    constructor() {
-        totalSupply = 1000000; // Initial supply of tokens
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply; // Assign all tokens to the contract deployer
+        totalSupply = _initialSupply; // Initial supply of tokens
+    }
+
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        balanceOf[msg.sender] -= _value; // Subtract from sender's balance
+        balanceOf[_to] += _value; // Add to recipient's balance
+        return true;
     }
 
 }
--- test/ShkToken.js
+++ test/ShkToken.js
@@ -1,13 +1,32 @@
 var ShkToken = artifacts.require("./ShkToken.sol");
 
-contract('ShkToken', function(accounts) {
+contract('ShkToken', function (accounts) {
+    var tokenInstance;
 
-    it('set the total supply upon deployment', function() {
-    return ShkToken.deployed().then(function(instance) {
-      tokenInstance = instance;
-      return tokenInstance.totalSupply();
-    }).then(function(totalSupply) {
-      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+    it('initializes the contract with the correct values', function () {
+        return ShkToken.deployed().then(function (instance) {
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function (name) {
+            assert.equal(name, 'Shk Token', 'has the correct name');
+            return tokenInstance.symbol();
+        }).then(function (symbol) {
+            assert.equal(symbol, 'Shk', 'has the correct symbol');
+            return tokenInstance.standard();
+        }).then(function (standard) {
+            assert.equal(standard, 'Shk Token v1.0', 'has the correct standard');
+        });
     });
+
+    it('allocates the initial supply upon deployment', function () {
+        return ShkToken.deployed().then(function (instance) {
+            tokenInstance = instance;
+            return tokenInstance.totalSupply();
+        }).then(function (totalSupply) {
+            assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function (adminBalance) {
+            assert.equal(adminBalance.toNumber(), 1000000, 'allocates the initial supply to the admin account');
+        });
     });
-})
+});
